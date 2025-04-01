c61f597c54225a4db0d0a4d16e3fd08c95fd2f7b
BattleGear04 <karekarayoosh@gmail.com>
2025-04-01 12:53:11 +0530

    added cors

--- backend/server.py
+++ backend/server.py
@@ -21,7 +21,7 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["*"],
+    allow_origins = ["https://sentisense-frontend.vercel.app"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
